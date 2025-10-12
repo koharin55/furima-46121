ddcfaec68996d6c93f2f7e7163f3f354d3a6b79c
koharin55 <ysugita0418@gmail.com>
2025-10-12 17:25:05 +0900

    ActiveHashの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,7 +5,7 @@ class ItemsController < ApplicationController
   def new
     @item = Item.new
   end
-  
+
 
   private
 
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,10 +1,16 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :shipping_fee
+  belongs_to :prefecture
+  belongs_to :shipping_day
 
   validates :name, presence: true
   validates :description, presence: true
-  validates :category_id, presence: true
+  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}, presence: true
   validates :condition_id, presence: true
   validates :shipping_fee_id, presence: true
   validates :prefecture_id, presence: true
